cbb2e7c8280f81fd286828576e1a28b837518938
Rafael Mio <rafamioads@gmail.com>
2020-06-20 19:33:41 -0300

    Estilo da tabela produtoras

--- resources/views/admin/produtoras/list.blade.php
+++ resources/views/admin/produtoras/list.blade.php
@@ -4,8 +4,8 @@
 
 <h1 class="my-3">Lista de produtoras</h1>
 
-<table class="table">
-    <thead>
+<table class="table table-bordered table-hover">
+    <thead class="bg-dark texto-laranja">
         <tr>
             <th>Id</th>
             <th>Nome</th>
@@ -17,7 +17,11 @@
         <tr>
             <td>{{$p->id}}</td>
             <td>{{$p->nome}}</td>
-            <td></td>
+            <td>
+                <a href="" class="btn btn-info">Exibir</a>
+                <a href="" class="btn btn-warning">Editar</a>
+                <a href="" class="btn btn-danger">Excluir</a>
+            </td>
         </tr>
         @endforeach
     </tbody>
