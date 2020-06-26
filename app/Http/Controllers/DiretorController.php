a4322ae7cf8f81bc526755c188b09e5129afe0e3
Rafael Mio <rafamioads@gmail.com>
2020-06-26 19:35:11 -0300

    Cadastro diretor

--- app/Http/Controllers/DiretorController.php
+++ app/Http/Controllers/DiretorController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Pais;
 use App\Diretor;
 use Illuminate\Http\Request;
 
@@ -10,5 +11,16 @@ class DiretorController extends Controller
     public function index(){
         $diretores = Diretor::with('pais')->paginate(5);
         return view('admin.diretores.index', ['diretores'=>$diretores]);
+    }
+
+    public function create(){
+        $paises = Pais::orderBy('nome', 'ASC')->get();
+        return view('admin.diretores.create',['paises'=>$paises]);
+    }
+
+    public function store(Request $request){
+        $dados = $request->all();
+        Diretor::create($dados);
+        return redirect()->action('DiretorController@index');
     }
 }
