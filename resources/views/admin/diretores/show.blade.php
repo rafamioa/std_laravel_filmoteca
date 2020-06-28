746066545c10668dfc13130eb8613bec3b139610
Rafael Mio <rafamioads@gmail.com>
2020-06-28 11:24:29 -0300

    Personalizada a página de exibit Diretor

--- resources/views/admin/diretores/show.blade.php
+++ resources/views/admin/diretores/show.blade.php
@@ -9,23 +9,37 @@
         <h1>Detalhes diretor: <b>{{ $diretor->nome }}</b></h1>
     </div>
     <div class="col-1">
-        <a href="{{ route('diretores.index') }}" class="btn btn-info ml-auto">Voltar</a>
+        <a href="{{ route('diretores.index') }}" class="btn btn-danger ml-auto">Voltar</a>
     </div>
 </div>
 <hr>
 
-<b>Foto: </b><br>
-@if($diretor->imagem === null)
-    <p> Não há foto </p>
-@else
-   <img src='{{ asset('storage/' . $diretor->imagem ) }}' alt='Foto do {{$diretor->nome}}' class='foto'> 
-@endif
+<div class="row">
+    <div class="col-4 text-center my-auto">
+        @if($diretor->imagem === null)
+        <p> Não há foto </p>
+    @else
+       <img src='{{ asset('storage/' . $diretor->imagem ) }}' alt='Foto do {{$diretor->nome}}' class='foto'> 
+    @endif
+    </div>
+    <div class="col-8">
+        <b>Nome: </b> {{ $diretor->nome }}<hr>
+        <b>Idade: </b> {{ $diretor->idade}} anos <hr>
+        <b>Data de Nascimento: </b> {{ date('d/m/Y', strtotime($diretor->data_nascimento)) }} <hr>
+        <b>País de origem: </b> {{ $diretor->pais->nome }}
+    </div>
+</div>
 <hr>
-<b>Nome: </b><p> {{ $diretor->nome }}</p><hr>
-<b>Biografia: </b><p> {{ $diretor->biografia }}</p><hr>
-<b>Idade: </b><p> {{ $diretor->idade}} anos</p><hr>
-<b>Data de Nascimento: </b><p> {{ date('d/m/Y', strtotime($diretor->data_nascimento)) }}</p><hr>
-<b>País de origem: </b><p> {{ $diretor->pais->nome }}</p><hr>
+<div>
+    <h1>Biografia</h1> 
+    <p>
+        {{$diretor->biografia}}
+    </p>
+</div>
+
+
+
+
 
 
 
